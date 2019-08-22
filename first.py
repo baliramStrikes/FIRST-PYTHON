249e4fc349bd1816b7b0b9204ce8e15e6c0169eb
Baliram <flacksingh@gmail.com>
2019-08-22 17:45:57 +0530

    version 1

---
+++ first.py
@@ -0,0 +1,11 @@
+# var1, var2, var3 =(input("sir what's your number ").split(","))
+# print ((int(var1) + int(var2) + int(var3)) / 3)
+
+# print ("sir your name is reversed  " + input('sir what\'s your name ')[::-1])
+
+
+# var1 = 'helloworld power'
+# print (var1.count("o"))
+
+var1, var2  = input('sir what is your name ').split(",")
+print (var1.upper().count(var2.upper().strip()))
